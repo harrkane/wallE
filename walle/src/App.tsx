e0c1f87f5d902c2cd97d746879fe7da836db0d7a
Aaron Kelley <akelly@neuronexus.com>
2019-09-13 22:23:24 -0500

    created typescript app and folders for components and css

---
+++ walle/src/App.tsx
@@ -0,0 +1,13 @@
+import React, {FC} from 'react';
+import './css/App.css';
+import Header from './components/Header';
+
+const App: FC = () => {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ walle/src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React, {FC} from 'react';
+import '../css/Header.css'
+
+
+const Header: FC = () => {
+    return (
+      <div className="Header">
+        wallE
+      </div>
+    );
+  }
+  
+  export default Header;
